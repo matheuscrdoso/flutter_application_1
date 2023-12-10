00d0549f716c567ab28eae28b82bdb86eb14b7bf
Pedro Lucas <79203933+pedrocslucas@users.noreply.github.com>
2023-12-10 14:57:56 -0300

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
-import 'information_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: InfoScreen(),
+      home: LoginScreen(),
     );
   }
 }
